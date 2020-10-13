6191bd5d5089c4eee49dc5dc99d270ae1a6d7dde
José Bollo <jose.bollo@iot.bzh>
2020-10-13 09:18:25 +0200

    filescan-utils: Improve header
    Avoid use of afb_api_rootdir_get_fd that is not defined
    for bindings v4
    
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

--- filescan-utils.h
+++ filescan-utils.h
@@ -99,13 +99,10 @@ extern char *GetAFBRootDirPath(afb_api_t apiHandle);
 /**
  * For compatibility issues :
  * 'GetBindingDirPath_' is linked to 'GetAFBRootDirPathUsingFd'
+ * 'GetBindingDirPath' is linked to 'GetAFBRootDirPath'
  */
-#define GetBindingDirPath_ GetAFBRootDirPathUsingFd
-
-static inline char *GetBindingDirPath(afb_api_t api)
-{
-  return GetAFBRootDirPathUsingFd(afb_api_rootdir_get_fd(api));
-}
+#define GetBindingDirPath_  GetAFBRootDirPathUsingFd
+#define GetBindingDirPath   GetAFBRootDirPath
 
 /*
  * @brief Get the Binding directory
